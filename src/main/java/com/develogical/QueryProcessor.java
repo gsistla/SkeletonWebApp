f3abe1b92ae2d995053cf4585274cf59865213ee
gsistla <67094603+gsistla@users.noreply.github.com>
2024-02-29 14:06:37 +0000

    Yen&Gayatri

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,6 +12,10 @@ public class QueryProcessor {
           + "writer in the English language and the world's pre-eminent dramatist.";
     }
 
+    if (query.toLowerCase().contains("What is your name?")) {
+      return "Yen & Gayatri";
+    }
+
     return "";
   }
 }
