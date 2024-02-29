3a2d18e1584c05d23dcaf1118e4fd8d981d9df99
gsistla <67094603+gsistla@users.noreply.github.com>
2024-02-29 14:14:25 +0000

    76

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,8 +6,8 @@ public class QueryProcessor {
 
     System.out.println("Received query:" + query);
 
-    if (query.toLowerCase().contains("Which of the following numbers is the largest: 87, 63, 10?")) {
-      return "87";
+    if (query.toLowerCase().contains("What is 22 plus 54?")) {
+      return "76";
     }
 
     return "";
